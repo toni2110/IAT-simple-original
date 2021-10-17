cd0196dc04c33cfe500d770bc301972ee2569453
toni2110 <92384247+toni2110@users.noreply.github.com>
2021-10-17 11:44:44 +0200

    Update IAT.js

--- IAT.js
+++ IAT.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://toni2110.github.io/IAT-simple-original/IAT-base.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
